5c52b58e6a7f61d7546fd0ae1e673e72984b7f27
Daniel Skurvidas <danielskurvidas@gmail.com>
2026-03-12 17:48:08 -0400

    Add priority attribute and getter/setter methods

--- job.py
+++ job.py
@@ -4,3 +4,27 @@ class Job:
         self.arrival = arrival
         self.burst = burst
         self.priority = priority
+
+    def getPid(self):
+        return self.pid
+
+    def getArrival(self):
+        return self.arrival
+
+    def getBurst(self):
+        return self.burst
+
+    def getPriority(self):
+        return self.priority
+
+    def setPid(self, pid):
+        self.pid = pid
+
+    def setArrival(self, arrival):
+        self.arrival = arrival
+
+    def setBurst(self, burst):
+        self.burst = burst
+
+    def setPriority(self, priority):
+        self.priority = priority
